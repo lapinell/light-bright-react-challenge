fff62144c85454977a18df867672a561e0dce0a7
Laura Pinell <lapinell@gmail.com>
2019-09-04 12:34:43 -0500

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+
+    entry: './src/index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    }
+};
